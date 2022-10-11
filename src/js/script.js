3776d4dc3726d1c1145f38950df4a9d38401b413
Ruslan Symonenko <symonenkors@gmail.com>
2022-10-11 16:04:00 +0300

    made header fixed by scrolling, added some anchors

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
+import {headerScroll} from './modules/headerScroll.js'
 import {anchors} from './modules/anchors.js';
 import {slider} from './modules/slider.js';
 
 window.addEventListener('DOMContentLoaded', () => {
+    headerScroll();
     anchors();
     slider();
 })
