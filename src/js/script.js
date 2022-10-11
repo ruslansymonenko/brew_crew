1ae3c03d2351a9ffc80dcc73d957b1494846c707
Ruslan Symonenko <symonenkors@gmail.com>
2022-10-11 20:18:53 +0300

    created slider for goods

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
-import {headerScroll} from './modules/headerScroll.js'
+import {headerScroll} from './modules/headerScroll.js';
 import {anchors} from './modules/anchors.js';
-import {slider} from './modules/slider.js';
+import {goodsSlider} from './modules/goodsSlider.js';
+import {infoSlider} from './modules/infoSlider.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     headerScroll();
     anchors();
-    slider();
+    goodsSlider('.goods_slider', '.goods_slider-line', '.goods_slider-item');
+    infoSlider('.info_page-slider', '.info_slider-line', '.info_page-slider-item');
 })
